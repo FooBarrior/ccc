115bfffa21fa4062f4ab1f19aa2a5e4d25efd19f
Nikita Malyavin <nikitamalyavin@gmail.com>
2011-12-01 06:24:56 +1100

    correct allocation style

--- lexer.c
+++ lexer.c
@@ -70,7 +70,7 @@ TokenPtr initToken(TokenPtr tok, LXR_TokenType type){
 
 TokenPtr initStrToken(StrTokenPtr tok, LXR_TokenType type, int len){
 	initToken((TokenPtr)tok, type);
-	tok->buff = malloc(len + 1);
+	tok->buff = NEW_SEQ(char, len + 1);
 	for(int i = 0; i < len; i++) tok->buff[i] = buff[i];
 	tok->buff[len] = 0;
 	tok->length = len;
