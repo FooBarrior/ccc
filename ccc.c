4ba37fb136e21ef4476bffa4248275d741fef765
Nikita Malyavin <nikitamalyavin@gmail.com>
2011-12-03 17:44:30 +1100

    add -v special for pousqie

--- ccc.c
+++ ccc.c
@@ -1,5 +1,16 @@
 #include "lexer.h"
 
 int main(int argc, char *argv[]){
+	char *read_args = "reading arguments...\n";
+	char *check_arg_v = "checking argument 0 for -v\n";
+	bool _v_found = false;
+	if(strcmp(argv[0], "-v")){
+		printf(read_args);
+		printf(check_arg_v);
+		printf("key -v found\n");
+
+		_v_found = true;
+	}
+	if(_v_found) printf("nothing to do. veaving...\n");
 	return 0;
 }
